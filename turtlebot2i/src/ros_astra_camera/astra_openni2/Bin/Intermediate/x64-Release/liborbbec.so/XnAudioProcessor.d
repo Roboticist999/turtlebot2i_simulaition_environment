5d39729a6314d80a77a8b5fa9f21eb014b0c76b7
Roboticist999 <zhang.zuc@husky.neu.edu>
2020-03-07 14:06:56 -0800

    able to run Gazebo and Rviz, not able to map environment

---
+++ turtlebot2i/src/ros_astra_camera/astra_openni2/Bin/Intermediate/x64-Release/liborbbec.so/XnAudioProcessor.d
@@ -0,0 +1,4 @@
+../../../Bin/Intermediate/x64-Release/liborbbec.so/XnAudioProcessor.d ../../../Bin/Intermediate/x64-Release/liborbbec.so/XnAudioProcessor.o: \
+ Sensor/XnAudioProcessor.cpp /usr/include/stdc-predef.h
+
+/usr/include/stdc-predef.h:
---
+++ turtlebot2i/src/ros_astra_camera/astra_openni2/Bin/Intermediate/x64-Release/liborbbec.so/XnSensorAudioStream.d
@@ -0,0 +1,4 @@
+../../../Bin/Intermediate/x64-Release/liborbbec.so/XnSensorAudioStream.d ../../../Bin/Intermediate/x64-Release/liborbbec.so/XnSensorAudioStream.o: \
+ Sensor/XnSensorAudioStream.cpp /usr/include/stdc-predef.h
+
+/usr/include/stdc-predef.h:
